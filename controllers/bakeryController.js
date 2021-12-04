2d292f92bbc63a3f36a658a77f3aa3de87d70e51
jessielista <jmgonzalez8282@gmail.com>
2021-12-04 09:43:21 -0800

    updating get and post

--- controllers/bakeryController.js
+++ controllers/bakeryController.js
@@ -5,21 +5,23 @@ const BakeryItem = require('../models/BakeryItems.js')
 // index route
 router.route('/').get((req, res) => {
   BakeryItem.find()
-    .then((foods) => res.json(foods))
+    .then((bakeryitems) => res.json(bakeryitems))
     .catch((err) => res.status(400).json('Error: ' + err))
+  // console.log('Request went through, now to retunrn something.')
+  // return res.send('Hello Jessica! /get')
 })
 
-//localhost:5000/bakery/
-
-http: router.route('/add').post((req, res) => {
+// new route
+router.route('/add').post((req, res) => {
+  // maybe need to use create?
   const bakeryItemName = req.body.bakeryItemName
-  const productDescription = req.body.productDescription
+  // const productDescription = req.body.productDescription
   const price = req.body.price
   // const price = Number(req.body.price)
 
   const newBakedGood = new BakeryItem({
     bakeryItemName,
-    productDescription,
+    // productDescription,
     price,
   })
 
@@ -27,6 +29,8 @@ http: router.route('/add').post((req, res) => {
     .save()
     .then(() => res.json('Baked good added!'))
     .catch((err) => res.status(400).json('Error: This is not working...' + err))
+
+  // return res.send(newBakedGood)
 })
 
 module.exports = router
